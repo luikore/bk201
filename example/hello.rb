79faf734df5d02126afb639817163c286b82ec02
LuiKore <usurffx@gmail.com>
2009-07-22 11:33:29 +0800

    change readme and add some comments to example

--- example/hello.rb
+++ example/hello.rb
@@ -1,9 +1,10 @@
 require File.expand_path(File.dirname __FILE__) + '/../bk201.rb'
 class A
   extend Bk201::Asm
-  asm_method :int, 'see_eax', 0 do |a|
-    # int2fix (i << 1 | 1)
-    a.code << "\xb8\x01\x00\x00\x00"
+  asm_method :int, 'zero', 0 do |a|
+    # mov eax, 1
+    # return value is 0 because int2fix (i << 1 | 1)
+    a.code << "\xb8\x01\x00\x00\x00" 
   end
 end
-puts A.new.see_eax
+puts A.new.zero
